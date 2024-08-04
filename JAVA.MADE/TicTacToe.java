ec8219799816411ea34d93309f0f61e2b2edb51a
chinmay4c <ghatuletejas@gmail.com>
2024-08-04 18:02:50 +0000

    adding functions

--- JAVA.MADE/TicTacToe.java
+++ JAVA.MADE/TicTacToe.java
@@ -3,23 +3,50 @@ import java.util.Scanner;
 public class TicTacToe {
 
     private static char[][] board = new char[3][3];
-    private static char currentPlayer = 'X';
+    private static char currentPlayer;
+    private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        initializeBoard();
-        printBoard();
         while (true) {
-            playerMove();
+            mainMenu();
+            initializeBoard();
+            currentPlayer = 'X';
             printBoard();
-            if (isWinner()) {
-                System.out.println("Player " + currentPlayer + " wins!");
+            while (true) {
+                playerMove();
+                printBoard();
+                if (isWinner()) {
+                    System.out.println("Player " + currentPlayer + " wins!");
+                    break;
+                }
+                if (isBoardFull()) {
+                    System.out.println("The game is a draw!");
+                    break;
+                }
+                switchPlayer();
+            }
+            if (!playAgain()) {
+                System.out.println("Thank you for playing!");
                 break;
             }
-            if (isBoardFull()) {
-                System.out.println("The game is a draw!");
+        }
+    }
+
+    private static void mainMenu() {
+        System.out.println("Welcome to Tic-Tac-Toe!");
+        System.out.println("1. Play");
+        System.out.println("2. Exit");
+        while (true) {
+            System.out.print("Enter your choice: ");
+            int choice = getValidInput(1, 2);
+            if (choice == 1) {
                 break;
+            } else if (choice == 2) {
+                System.out.println("Goodbye!");
+                System.exit(0);
+            } else {
+                System.out.println("Invalid choice. Please choose 1 or 2.");
             }
-            switchPlayer();
         }
     }
 
@@ -32,7 +59,9 @@ public class TicTacToe {
     }
 
     private static void printBoard() {
+        System.out.println("  0 1 2");
         for (int i = 0; i < 3; i++) {
+            System.out.print(i + " ");
             for (int j = 0; j < 3; j++) {
                 System.out.print(board[i][j] + " ");
             }
@@ -41,19 +70,55 @@ public class TicTacToe {
     }
 
     private static void playerMove() {
-        Scanner scanner = new Scanner(System.in);
         int row, col;
         while (true) {
             System.out.println("Player " + currentPlayer + ", enter your move (row and column): ");
-            row = scanner.nextInt();
-            col = scanner.nextInt();
-            if (row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] == '-') {
+            row = getValidInput("Row");
+            col = getValidInput("Column");
+            if (board[row][col] == '-') {
                 board[row][col] = currentPlayer;
                 break;
             } else {
-                System.out.println("This move is not valid");
+                System.out.println("This move is not valid. The cell is already occupied.");
             }
         }
+    }
+
+    private static int getValidInput(String coordinate) {
+        int value;
+        while (true) {
+            System.out.print(coordinate + " (0, 1, or 2): ");
+            if (scanner.hasNextInt()) {
+                value = scanner.nextInt();
+                if (value >= 0 && value <= 2) {
+                    break;
+                } else {
+                    System.out.println("Invalid " + coordinate + ". Please enter 0, 1, or 2.");
+                }
+            } else {
+                System.out.println("Invalid input. Please enter a number.");
+                scanner.next(); // Clear invalid input
+            }
+        }
+        return value;
+    }
+
+    private static int getValidInput(int min, int max) {
+        int value;
+        while (true) {
+            if (scanner.hasNextInt()) {
+                value = scanner.nextInt();
+                if (value >= min && value <= max) {
+                    break;
+                } else {
+                    System.out.println("Invalid input. Please enter a number between " + min + " and " + max + ".");
+                }
+            } else {
+                System.out.println("Invalid input. Please enter a number.");
+                scanner.next(); // Clear invalid input
+            }
+        }
+        return value;
     }
 
     private static void switchPlayer() {
@@ -85,5 +150,19 @@ public class TicTacToe {
             }
         }
         return true;
+    }
+
+    private static boolean playAgain() {
+        System.out.println("Do you want to play again? (yes or no)");
+        while (true) {
+            String response = scanner.next();
+            if (response.equalsIgnoreCase("yes")) {
+                return true;
+            } else if (response.equalsIgnoreCase("no")) {
+                return false;
+            } else {
+                System.out.println("Invalid input. Please enter 'yes' or 'no'.");
+            }
+        }
     }
 }
